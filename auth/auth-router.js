2bc46774d45fd045f691b5c3b2cfc66fce952e71
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-13 14:54:38 +0100

    Send a jwt to a logged in user
    In this commit, a jwt token, along with awelcome message, is sent back to a user upon successful login.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -26,8 +26,10 @@ router.post('/register', (req, res) => {
       .first()
       .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
+            const token = generateToken(user);
           res.status(200).json({
-            message: `Welcome ${user.username}!`
+            message: `Welcome ${user.username}!`,
+            token
           });
         } else {
           res.status(401).json({ message: 'Invalid Credentials' });
@@ -37,4 +39,22 @@ router.post('/register', (req, res) => {
         res.status(500).json(error);
       });
   });
+
+  function generateToken(user) {
+    const payload = {
+      subject: user.id,
+      username: user.username,
+    }
+    const options = {
+      expiresIn: '1d',
+    }
+  
+    const result = jwt.sign(
+      payload,
+      'shhhhhh!!!! keep it secret!',
+      options,
+    )
+  
+    return result;
+  }
 module.exports = router;
