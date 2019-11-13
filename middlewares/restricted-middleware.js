5a9a54bbbacfe4e24cbbfcc8dc26dae4c8dff3f8
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-13 16:51:25 +0100

    Add validateUser and restricted middlewares
    In this commit, I created a file called middleware inside the middlewares folder.
    This file exports all middlewares to be used in all api endpoints.
    I added validateNewUser() and restricted() middleware functions for validating a new user which will be registered and to restrict access to all protected resource endpoints respectively

---
+++ middlewares/restricted-middleware.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken");
+
+const secret = "shhhhhh!!!! keep it secret!";
+
+module.exports = {
+  restricted,
+  validateNewUser
+};
+
+function restricted(req, res, next) {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: err.message });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "No credentials provided" });
+  }
+}
+
+function validateNewUser(req, res, next) {
+  const newUserToBeRegistered = req.body;
+  if (!newUserToBeRegistered) {
+    res
+      .status(400)
+      .json({ message: `All new users must have required fields` });
+  } else if (!newUserToBeRegistered.username) {
+    res.status(400).json({ message: `User must have a username field` });
+  } else if (!newUserToBeRegistered.password) {
+    res.status(400).json({ message: "User must have a password field" });
+  } 
+  else if (!newUserToBeRegistered.department) {
+    res.status(400).json({ message: "User must have a department field" });
+  }else {
+    req.user = newUserToBeRegistered;
+    next();
+  }
+}
