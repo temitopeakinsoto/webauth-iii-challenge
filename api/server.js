2e2fafc1d7e8453c21add8ced64707f38905ddfc
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-13 13:57:46 +0100

    Add a barebone server.js file for routing requests to our API endpoints

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+
+server.get('/', (req, res) => {
+  res.json({ api: 'Yaay! our api is working' });
+});
+
+module.exports = server;
